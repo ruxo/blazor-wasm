856110aecb7950fe216b8a09eb9d7564d5afa0d5
Ruxo Zheng <rz@just.sent.as>
2022-07-10 09:25:31 +0700

    Save code on Client side

---
+++ src/Client/Features/ManageTrails/AddTrailHandler.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using System.Net.Http.Json;
+using Wasm.Shared.Features.ManageTrails;
+
+namespace Wasm.Client.Features.ManageTrails;
+
+// ReSharper disable once UnusedType.Global
+public sealed class AddTrailHandler : IRequestHandler<AddTrailRequest, AddTrailRequest.Response>
+{
+    readonly HttpClient http;
+
+    public AddTrailHandler(HttpClient http) {
+        this.http = http;
+    }
+
+    public async Task<AddTrailRequest.Response> Handle(AddTrailRequest request, CancellationToken cancellationToken) {
+        var response = await http.PostAsJsonAsync(AddTrailRequest.RouteTemplate, request, cancellationToken);
+        if (response.IsSuccessStatusCode) {
+            var trailId = await response.Content.ReadFromJsonAsync<int>(cancellationToken: cancellationToken);
+            return new(trailId);
+        }
+        else
+            return new(null);
+    }
+}
--- src/Client/Program.cs
+++ src/Client/Program.cs
@@ -1,3 +1,4 @@
+using MediatR;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Wasm.Client;
@@ -7,5 +8,6 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient{ BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddMediatR(typeof(Program).Assembly);
 
 await builder.Build().RunAsync();
