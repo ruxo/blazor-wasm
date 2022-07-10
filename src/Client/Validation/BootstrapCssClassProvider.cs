19ec31bb4d93dfab9cbc655fc67a56be1f0c180c
Ruxo Zheng <rz@just.sent.as>
2022-07-10 20:05:15 +0700

    Use custom CSS class provider

---
+++ src/Client/Validation/BootstrapCssClassProvider.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+
+namespace Wasm.Client.Validation;
+
+public sealed class BootstrapCssClassProvider : FieldCssClassProvider
+{
+    public override string GetFieldCssClass(EditContext editContext, in FieldIdentifier fieldIdentifier) {
+        var isValid = !editContext.GetValidationMessages(fieldIdentifier).Any();
+        var isModified = editContext.IsModified(fieldIdentifier);
+        return isValid ? isModified ? "is-valid" : "" : "is-invalid";
+    }
+}
