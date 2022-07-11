607495e179220e0749f0ad1691e82b01f10718bb
Ruxo Zheng <rz@just.sent.as>
2022-07-11 20:24:05 +0700

    Handle file upload on client side

---
+++ src/Client/Features/ManageTrails/UploadTrailImageHandler.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using Wasm.Shared.Features.ManageTrails;
+
+namespace Wasm.Client.Features.ManageTrails;
+
+// ReSharper disable once UnusedType.Global
+public sealed class UploadTrailImageHandler : IRequestHandler<UploadTrailImageRequest, UploadTrailImageRequest.Response>
+{
+    readonly HttpClient http;
+    public UploadTrailImageHandler(HttpClient http) {
+        this.http = http;
+    }
+
+    public async Task<UploadTrailImageRequest.Response> Handle(UploadTrailImageRequest request, CancellationToken cancellationToken) {
+        var fileContent = request.File.OpenReadStream(request.File.Size, cancellationToken);
+
+        using var content = new MultipartFormDataContent();
+        content.Add(new StreamContent(fileContent), "image", request.File.Name);
+
+        var targetUri = UploadTrailImageRequest.RouteTemplate.Replace("{trailId}", request.TrailId.ToString());
+        var response = await http.PostAsync(targetUri, content, cancellationToken);
+
+        if (response.IsSuccessStatusCode) {
+            var fileName = await response.Content.ReadAsStringAsync(cancellationToken: cancellationToken);
+            return new(fileName);
+        }
+        else
+            return new(null);
+    }
+}
---
+++ src/Shared/Features/ManageTrails/UploadTrailImageRequest.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace Wasm.Shared.Features.ManageTrails;
+
+public record UploadTrailImageRequest(int TrailId, IBrowserFile File) : IRequest<UploadTrailImageRequest.Response>
+{
+    public const string RouteTemplate = "/api/trails/{trailId}/images";
+    public record Response(string? ImageName);
+}
