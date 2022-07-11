2f0d9d63a4daf9dea1a0d67e0540fb6a45bff428
Ruxo Zheng <rz@just.sent.as>
2022-07-12 05:09:25 +0700

    Refactor TrailForm from AddTrailPage

--- src/Client/Features/ManageTrails/AddTrail/AddTrailHandler.cs
+++ src/Client/Features/ManageTrails/AddTrail/AddTrailHandler.cs
@@ -2,20 +2,23 @@
 using System.Net.Http.Json;
 using Wasm.Shared.Features.ManageTrails;
 
-namespace Wasm.Client.Features.ManageTrails;
+namespace Wasm.Client.Features.ManageTrails.AddTrail;
 
 // ReSharper disable once UnusedType.Global
 public sealed class AddTrailHandler : IRequestHandler<AddTrailRequest, AddTrailRequest.Response>
 {
     readonly HttpClient http;
 
-    public AddTrailHandler(HttpClient http) {
+    public AddTrailHandler(HttpClient http)
+    {
         this.http = http;
     }
 
-    public async Task<AddTrailRequest.Response> Handle(AddTrailRequest request, CancellationToken cancellationToken) {
+    public async Task<AddTrailRequest.Response> Handle(AddTrailRequest request, CancellationToken cancellationToken)
+    {
         var response = await http.PostAsJsonAsync(AddTrailRequest.RouteTemplate, request, cancellationToken);
-        if (response.IsSuccessStatusCode) {
+        if (response.IsSuccessStatusCode)
+        {
             var trailId = await response.Content.ReadFromJsonAsync<int>(cancellationToken: cancellationToken);
             return new(trailId);
         }
